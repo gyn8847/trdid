80ea456346d5b5cae801dfe181d9ef7209376f1e
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 18:57:56 +1200

    Update focuspop.php

--- focuspop.php
+++ focuspop.php
@@ -1,29 +1,30 @@
 <?php
 	session_start();
 	$year = $_SESSION["year"];
+	$pop = $_SESSION["pop"];
+	$mil = $_SESSION["mil"];
+	$eco = $_SESSION["eco"];
+	$tech = $_SESSION["tech"];
+
 	$year++;
 	$year = floor($year);
 	$_SESSION["year"] = $year;
 
-	$pop = $_SESSION["pop"];
 	$pop = $pop + rand(5,20) + ($pop / 1000);
 	$pop = floor($pop);
 	$_SESSION["pop"] = $pop;
 
-	$mil = $_SESSION["mil"];
 	$mil = $mil + rand(0,1) + ($pop / 30000);
 	$mil = floor($mil);
 	$_SESSION["mil"] = $mil;
 
-	$eco = $_SESSION["eco"];
-	$eco = $eco + $tech + rand(1,4) - ($pop / 5000);
+	$eco = $eco + ($tech + rand(1,(4+sqrt($tech))) - ($pop / 5000);
 	$eco = floor($eco);
 	if ($eco < 2){
 	$eco == 2;
 	}
 	$_SESSION["eco"] = $eco;
 
-	$tech = $_SESSION["tech"];
 	$tech = $tech + ($eco / 100);
 	$tech = floor($tech);
 	$_SESSION["tech"] = $tech;
