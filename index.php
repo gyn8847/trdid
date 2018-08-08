4e9aef6ca843f954aa2b50d9fc28421749e10cb8
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 15:28:49 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+
+<?php
+ 	session_start();
+ 	if (!isset ($_SESSION["number"])) {
+ 	$_SESSION["number"] = 0;
+ 	}
+ 	$num = $_SESSION["number"];
+?>
+
 <html>
 <head>
 <title>Page Title</title>
