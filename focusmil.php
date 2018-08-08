d782e34f2c67f2f7bd8ffefcdb9890ff61b95bed
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 19:14:36 +1200

    Update focusmil.php

--- focusmil.php
+++ focusmil.php
@@ -1,23 +1,27 @@
 <?php
 
 	session_start();
-  
-	$year = $_SESSION["year"];
+  	$year = $_SESSION["year"];
+	$pop = $_SESSION["pop"];
+	$mil = $_SESSION["mil"];
+	$eco = $_SESSION["eco"];
+	$tech = $_SESSION["tech"];
+	
 	$year++;
 	$year = floor($year);
  	$_SESSION["year"] = $year;
 
-	$pop = $_SESSION["pop"];
+	
 	$pop = $pop + rand(3,15) + ($pop / 1500);
 	$pop = floor($pop);
 	$_SESSION["pop"] = $pop;
   
-	$mil = $_SESSION["mil"];
+	
 	$mil = $mil + rand(0,2) + ($pop / 15000);
 	$mil = floor($mil);
 	$_SESSION["mil"] = $mil;
   
-	$eco = $_SESSION["eco"];
+	
 	$eco = $eco + $tech + rand(1,4) - ($pop / 5000);
 	$eco = floor($eco);
 	if ($eco < 2){
@@ -25,7 +29,7 @@
 	}
 	$_SESSION["eco"] = $eco;
   
-	$tech = $_SESSION["tech"];
+	
 	$tech = $tech + ($eco / 100);
 	$tech = floor($tech);
 	$_SESSION["tech"] = $tech;
