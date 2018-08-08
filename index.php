cd721801de05b39d7f46ece154a81cf210a934b2
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 15:38:23 +1200

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 <?php
  	session_start();
- 	if (!isset ($_SESSION["number"])) {
- 	$_SESSION["number"] = 0;
+	$techstate = "Ancient";
+ 	if (!isset ($_SESSION["year"])) {
+ 	$_SESSION["year"] = 0;
  	}
- 	$num = $_SESSION["number"];
+ 	$year = $_SESSION["year"];
+	if (!isset ($_SESSION["pop"])) {
+ 	$_SESSION["pop"] = 5000;
+ 	}
+ 	$pop = $_SESSION["pop"];
+	if (!isset ($_SESSION["mil"])) {
+ 	$_SESSION["mil"] = 0;
+ 	}
+ 	$mil = $_SESSION["mil"];
+	if (!isset ($_SESSION["eco"])) {
+ 	$_SESSION["eco"] = 0;
+ 	}
+ 	$eco = $_SESSION["eco"];
+	if (!isset ($_SESSION["tech"])) {
+ 	$_SESSION["tech"] = 0;
+ 	}
+ 	$tech = $_SESSION["tech"];
 ?>
 
 <html>
@@ -15,11 +32,11 @@
 <body>
 <p>$event</p>
 <?php
-   echo "<p>The year is: $num</p></br>";
- 		echo "<p>Population: $num</p>";
-   echo "<p>Soldiers: $num</p>";
-   echo "<p>GDP per capita: $num</p>";
-   echo "<p>Your tech level is: $num</p>";
+   echo "<p>The year is: $year</p></br>";
+   echo "<p>Population: $pop</p>";
+   echo "<p>Soldiers: $mil</p>";
+   echo "<p>GDP per capita: $eco</p>";
+   echo "<p>Your tech level is: $techstate</p>";
 	?>
 <p><a href="popfocus.php">Focus Population</a></p>
 <p><a href="milfocus.php">Focus Soldiers</a></p>
