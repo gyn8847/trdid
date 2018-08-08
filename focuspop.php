78a958a93e09eb073e8b1d3987f948f990aeb492
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 15:50:52 +1200

    Update focuspop.php

--- focuspop.php
+++ focuspop.php
@@ -1,20 +1,8 @@
 <?php
 	session_start();
-
-	$eventnum = rand(1,2);
-	if(rand == 1){
 	$event = "You experience a fruitful harvest"	
 	$pop = $_SESSION["pop"];
 	$pop = $pop + ($pop / 1000) + rand(15,25));
 	$_SESSION["pop"] = $pop;
-	}
-	elseif(rand == 2){
-	$event = "You experience a poor harvest"	
-	$pop = $_SESSION["pop"];
-	$pop = $pop + ($pop / 5000) + rand(5,15));
-	$_SESSION["pop"] = $pop;
-	}
-	
 	header("location:index.php");
-	
 ?>
