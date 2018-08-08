eb975474458b30559a3e018d665ecd11ee340917
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 17:38:10 +1200

    Update focuspop.php

--- focuspop.php
+++ focuspop.php
@@ -1,8 +1,7 @@
 <?php
 	session_start();
-	$event = "You experience a fruitful harvest";	
 	$pop = $_SESSION["pop"];
-	$pop = $pop + ($pop / 1000) + rand(15,25));
-	$_SESSION["pop"] = $pop;
-	header("location:index.php");
+	$pop++;
+	$_SESSION["number"] = $pop;
+	header("location:number.php");
 ?>
