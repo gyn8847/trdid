845f796b9c29e02b993086a7c7f7c8426c7f28ee
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 16:08:36 +1200

    Update focuspop.php

--- focuspop.php
+++ focuspop.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$event = "You experience a fruitful harvest"	
+	$event = "You experience a fruitful harvest";	
 	$pop = $_SESSION["pop"];
 	$pop = $pop + ($pop / 1000) + rand(15,25));
 	$_SESSION["pop"] = $pop;
