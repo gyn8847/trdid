40a366cebac7796fd2b279644abdc2898d371e7a
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 19:20:09 +1200

    Update focuseco.php

--- focuseco.php
+++ focuseco.php
@@ -1,31 +1,33 @@
 <?php
 
 	session_start();
-  
-	$year = $_SESSION["year"];
+  	$year = $_SESSION["year"];
+	$pop = $_SESSION["pop"];
+	$mil = $_SESSION["mil"];
+	$eco = $_SESSION["eco"];
+	$tech = $_SESSION["tech"];
+	
 	$year++;
 	$year = floor($year);
   	$_SESSION["year"] = $year;
 
-	$pop = $_SESSION["pop"];
+	
 	$pop = $pop + rand(3,15) + ($pop / 1500);
 	$pop = floor($pop);
 	$_SESSION["pop"] = $pop;
   
-	$mil = $_SESSION["mil"];
+	
 	$mil = $mil + rand(0,1) + ($pop / 30000);
 	$mil = floor($mil);
 	$_SESSION["mil"] = $mil;
   
-	$eco = $_SESSION["eco"];
-	$eco = $eco + $tech + rand(1,7) - ($pop / 5000);
+	$eco = $eco + $tech + rand(1,(7+sqrt($tech*1.5))) - ($pop / 5000);
 	$eco = floor($eco);
 	if ($eco < 2){
 	$eco == 2;
 	}
 	$_SESSION["eco"] = $eco;
   
-	$tech = $_SESSION["tech"];
 	$tech = $tech + ($eco / 100);
 	$tech = floor($tech);
 	$_SESSION["tech"] = $tech;
