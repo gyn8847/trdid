8b63b386890d8479e38c35683b4bf3a873bc8fc4
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-08 19:03:38 +1200

    Update focustech.php

--- focustech.php
+++ focustech.php
@@ -19,7 +19,7 @@
 	$mil = floor($mil);
 	$_SESSION["mil"] = $mil;
   
-	$eco = $eco + $tech + rand(1,4) - ($pop / 5000);
+	$eco = $eco + $tech + rand(1,(4+sqrt($tech))) - ($pop / 5000);
 	$eco = floor($eco);
 	if ($eco < 2){
 	$eco == 2;
@@ -73,6 +73,16 @@
 	elseif($tech >= 1000){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Copper Age";
+	$_SESSION["techstate"] = $techstate;
+	}
+	else($tech >= 500){
+	$techstate = $_SESSION["techstate"];
+	$techstate = "Neolithic Age";
+	$_SESSION["techstate"] = $techstate;
+	}
+	else($tech >= 0){
+	$techstate = $_SESSION["techstate"];
+	$techstate = "Prehistoric Age";
 	$_SESSION["techstate"] = $techstate;
 	}
   
