7aba3871329fddb1ab5947d3d82b6bb522bb8bc8
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-09 20:16:46 +1200

    Update focus.php

--- focus.php
+++ focus.php
@@ -6,33 +6,24 @@
 	$mil = $_SESSION["mil"];
 	$eco = $_SESSION["eco"];
 	$tech = $_SESSION["tech"];
+	$civpointpop = $_SESSION["civpointpop"];
+	$civpointmil = $_SESSION["civpointmil"];
+	$civpointeco = $_SESSION["civpointeco"];
+	$civpointtech = $_SESSION["civpointtech"];
 
 	$year++;
 	$year = floor($year);
   	$_SESSION["year"] = $year;
 	
-	$pop = $pop + rand(3,15) + ($pop / 750);
+	$pop = $pop + rand(3,15+($civpointpop*2)) + ($pop / (750-($civpointpop*15)));
 	$pop = floor($pop);
 	$_SESSION["pop"] = $pop;
   
-	$mil = $mil + rand(0,1) + ($pop / 30000);
+	$mil = $mil + rand(0,1+($civpointmil/2)) + ($pop / (30000-($civpointmil*4000)));
 	$mil = floor($mil);
 	$_SESSION["mil"] = $mil;
   
-	$techstatescore = 1;
-	if ($techstate = "Prehistoric Age"){
-	}
-	elseif ($techstate = "Neolithic Age"){
-		$techstatescore = $techstatescore + 1;
-	}
-	elseif ($techstate = "Copper Age"){
-		$techstatescore = $techstatescore + 2;
-	}
-	elseif ($techstate = "Bronze Age"){
-		$techstatescore = $techstatescore + 3;
-	}
-
-	$eco = $eco + $tech + rand(1,(4+($techstatescore))) - ($pop / 5000);
+	$eco = $eco + $tech + rand(1,(4+($civpointeco))) - ($pop / 5000);
 	$eco = floor($eco);
 	if ($eco < 2){
 	$eco == 2;
