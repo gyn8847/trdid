b7ab20593f0fea7a5b6ef77fb60268164d6c83ae
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-11 02:59:17 +1200

    Update index.php

--- index.php
+++ index.php
@@ -103,10 +103,10 @@ var x = setInterval(function() {
     var distance = startpoint - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(now / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((now % (1000 * 60)) / 1000);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
