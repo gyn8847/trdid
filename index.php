02db8849b447720f55cec3547f1c664e1d205c8d
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-11 04:05:13 +1200

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ var x = setInterval(function() {
     var distance = now - startpoint;
     
     // Time calculations for days, hours, minutes and seconds
-    var yeartimer = Math.floor((distance / 60));
+    var yeartimer = Math.floor((distance / 10));
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
