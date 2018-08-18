976f139a2f583a93c7d8809cd49f58847363c20f
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-18 21:05:05 +1200

    Update focus.php

--- focus.php
+++ focus.php
@@ -1,94 +1,94 @@
 <?php
 	session_start();
   
-	$year = $_SESSION["year"];
-	$pop = $_SESSION["pop"];
-	$mil = $_SESSION["mil"];
-	$eco = $_SESSION["eco"];
-	$tech = $_SESSION["tech"];
-	$civpointpop = $_SESSION["civpointpop"];
-	$civpointmil = $_SESSION["civpointmil"];
-	$civpointeco = $_SESSION["civpointeco"];
-	$civpointtech = $_SESSION["civpointtech"];
+	$year = $_GET["year"];
+	$pop = $_GET["pop"];
+	$mil = $_GET["mil"];
+	$eco = $_GET["eco"];
+	$tech = $_GET["tech"];
+	$civpointpop = $_GET["civpointpop"];
+	$civpointmil = $_GET["civpointmil"];
+	$civpointeco = $_GET["civpointeco"];
+	$civpointtech = $_GET["civpointtech"];
 
 	$year++;
 	$year = floor($year);
-  	$_SESSION["year"] = $year;
+  	$_GET["year"] = $year;
 	
 	$pop = $pop + rand(3,15+($civpointpop*2)) + ($pop / (750-($civpointpop*15)));
 	$pop = floor($pop);
-	$_SESSION["pop"] = $pop;
+	$_GET["pop"] = $pop;
   
 	$mil = $mil + rand(0,1+($civpointmil/2)) + ($pop / (30000-($civpointmil*4000)));
 	$mil = floor($mil);
-	$_SESSION["mil"] = $mil;
+	$_GET["mil"] = $mil;
   
 	$eco = $eco + rand(0,(4+($civpointeco/2))) - ($pop / (5000+($tech*10)));
 	$eco = floor($eco);
 	if ($eco < 2){
 	$eco == 2;
 	}
-	$_SESSION["eco"] = $eco;
+	$_GET["eco"] = $eco;
   
 	$tech = $tech + (sqrt($eco/(10-$civpointtech)));
 	$tech = floor($tech);
-	$_SESSION["tech"] = $tech;
+	$_GET["tech"] = $tech;
 
 	if($tech >= 150000){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Futuristic Age";
-	$_SESSION["tech"] = $techstate;
+	$_GET["tech"] = $techstate;
 	}
 	elseif($tech >= 50000){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Space Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 20000){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Modern Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 8000){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Industrial Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 3500){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Exploration Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 1500){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Medieval Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 700){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Iron Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 300){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Bronze Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 150){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Copper Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	elseif($tech >= 50){
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Neolithic Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
 	else{
 	$techstate = $_SESSION["techstate"];
 	$techstate = "Prehistoric Age";
-	$_SESSION["techstate"] = $techstate;
+	$_GET["techstate"] = $techstate;
 	}
   
-	header("location:index.php");
+	//header("location:index.php");
 ?>
